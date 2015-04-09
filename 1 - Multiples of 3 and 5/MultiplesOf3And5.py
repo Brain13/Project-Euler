d4a35ad14939da0664e1e94cad2262ee775e7395
Brian <BrianKlinect@gmail.com>
2015-04-09 19:05:36 -0400

    Upped it to Project Euler params

--- 1 - Multiples of 3 and 5/MultiplesOf3And5.py
+++ 1 - Multiples of 3 and 5/MultiplesOf3And5.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-MAX_NUMBER = 10
+MAX_NUMBER = 1000
 
 runningTotal = 0
 
+# brute force to loop through
 for i in range(1, MAX_NUMBER):
     if i % 3 == 0:
         runningTotal += i
     elif i % 5 == 0:
         runningTotal += i
         
-        
+# print the result for reporting
 print(runningTotal)
 
